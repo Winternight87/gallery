897f26ee885983d0bb22adf5da4cf7737eb1c132
Winter Night <winter.night987@ya.ru>
2017-02-13 17:38:38 +0300

    Tooltip Improvements
    Intermediate update. To do:
    - remove EventListener;
    - wrap all in anon function

--- photostorage/photogallery/static/js/main.js
+++ photostorage/photogallery/static/js/main.js
@@ -1,10 +1,40 @@
-function showTooltip(x) {
-  Tooltip = x.childNodes[1];
-  document.addEventListener('mousemove', fn, false);
-  
-  function fn(e) {
-    Tooltip.style.left = e.pageX + 10 + 'px';
-    Tooltip.style.top = e.pageY + 20 + 'px'; 
+//(function() {
+  function initializeTooltip(dataTitle, dataDate) {
+    var tooltipFrame = document.createElement("div"),
+        tooltipTitle = document.createTextNode(dataTitle),
+        tooltipDate = document.createTextNode(dataDate);
+    
+    tooltipFrame.setAttribute("id", "tooltip-frame");
+    tooltipFrame.appendChild(tooltipTitle);
+    tooltipFrame.appendChild(document.createElement("br"));
+    tooltipFrame.appendChild(tooltipDate);
+    
+    document.querySelector(".content").appendChild(tooltipFrame);
+    
+    return tooltipFrame;
   }
   
-}
+  function showTooltip(x) {
+    var tooltipDate = x.dataset.date,
+        tooltipTitle = x.dataset.title;
+
+    curTooltip = initializeTooltip(tooltipTitle, tooltipDate);
+    moveTooltip(curTooltip);
+  }
+  
+  function moveTooltip(tooltipFrame) {
+    document.addEventListener('mousemove', move, false);
+    function move(e) {
+      tooltipFrame.style.left = e.pageX + 10 + 'px';
+      tooltipFrame.style.top = e.pageY + 20 + 'px'; 
+    }
+  }  
+
+  function hideTooltip() {
+    var tooltipFrame = document.getElementById("tooltip-frame");
+    tooltipFrame.parentNode.removeChild(tooltipFrame);
+    //document.removeEventListener('mousemove', move, false);
+  }
+
+//})();
+
