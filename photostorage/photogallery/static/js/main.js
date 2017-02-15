7090caa90f1d45f380487c82b959b724b99d73ed
Winter Night <winter.night987@ya.ru>
2017-02-15 12:05:54 +0300

    JS made unobtrusive

--- photostorage/photogallery/static/js/main.js
+++ photostorage/photogallery/static/js/main.js
@@ -1,40 +1,36 @@
-//(function() {
-  function initializeTooltip(dataTitle, dataDate) {
-    var tooltipFrame = document.createElement("div"),
-        tooltipTitle = document.createTextNode(dataTitle),
-        tooltipDate = document.createTextNode(dataDate);
-    
-    tooltipFrame.setAttribute("id", "tooltip-frame");
-    tooltipFrame.appendChild(tooltipTitle);
-    tooltipFrame.appendChild(document.createElement("br"));
-    tooltipFrame.appendChild(tooltipDate);
-    
-    document.querySelector(".content").appendChild(tooltipFrame);
-    
-    return tooltipFrame;
+function initializeGallery()
+{
+  var imgs,i;
+  imgs = document.getElementsByClassName("preview-frame");
+  for(i=0;i<imgs.length;i++)
+  {
+    imgs[i].onmouseover=function(){showTooltip(this);};
+    imgs[i].onmouseout=function(){hideTooltip(this);};
   }
-  
-  function showTooltip(x) {
-    var tooltipDate = x.dataset.date,
-        tooltipTitle = x.dataset.title;
+}
 
-    curTooltip = initializeTooltip(tooltipTitle, tooltipDate);
-    moveTooltip(curTooltip);
-  }
-  
-  function moveTooltip(tooltipFrame) {
-    document.addEventListener('mousemove', move, false);
-    function move(e) {
-      tooltipFrame.style.left = e.pageX + 10 + 'px';
-      tooltipFrame.style.top = e.pageY + 20 + 'px'; 
-    }
-  }  
+function showTooltip(x) {
+  document.getElementById("tooltip-title").innerHTML = x.dataset.title;
+  document.getElementById("tooltip-date").innerHTML = x.dataset.date;
+  document.getElementById("tooltip-frame").style.display = "block";
+  document.addEventListener('mousemove', moveTooltip, false);
+}
 
-  function hideTooltip() {
-    var tooltipFrame = document.getElementById("tooltip-frame");
-    tooltipFrame.parentNode.removeChild(tooltipFrame);
-    //document.removeEventListener('mousemove', move, false);
-  }
+function hideTooltip(x) {
+  document.getElementById("tooltip-frame").style.display = "none";
+  document.removeEventListener('mousemove', moveTooltip, false);
+}
 
-//})();
+function moveTooltip(e) {
+  tooltipFrame = document.getElementById("tooltip-frame")
+  tooltipFrame.style.left = e.pageX + 10 + 'px';
+  tooltipFrame.style.top = e.pageY + 20 + 'px';  
+}
+
+
+window.onload=function(){
+  initializeGallery();
+}
+
+
 
